a1641d082b7741a16a4677d462c09c7c6b3f25a0
eivan <102188631+ivankosz@users.noreply.github.com>
2022-10-03 12:25:59 -0300

    Delete clase 14/clase 14 directory

--- clase 14/clase 14/ej1.py
+++
@@ -1,9 +0,0 @@
-
-user=int(input("Ingrese un num: "))
-numeros=list()
-while user != -1:
-    numeros.append(user)
-    user=int(input("Ingrese un num: "))
-numeros.reverse()
-for i in numeros:
-    print (i)
--- clase 14/clase 14/ej3.py
+++
@@ -1,9 +0,0 @@
-
-lista=list()    
-while True:
-    user=input('pkm: ')
-    lista.append(user)
-    if user == 'ekans':
-        break
-for i in lista:
-    print(i.capitalize(), i[::-1].capitalize())
