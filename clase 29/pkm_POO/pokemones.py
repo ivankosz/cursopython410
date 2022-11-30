6762552c1d4638294cd09c890d82a75307eb05f7
eivan <102188631+ivankosz@users.noreply.github.com>
2022-11-30 11:42:16 -0300

    Add files via upload

--- clase 29/pkm_POO/pokemones.py
+++ clase 29/pkm_POO/pokemones.py
@@ -19,7 +19,7 @@ class Pokemon:
             return f"daño recibido: {self.salud}, salud: {self.salud}\n{self.nombre} ha sido derrotado"
         else:
             self.salud -= danho
-            return f"daño recibido: {danho}, salud: {self.salud}"
+            return f"{self.nombre}, daño recibido: {danho}, salud: {self.salud}"
 
 
 class Ratata (Pokemon, Normal):
@@ -30,7 +30,7 @@ class Ratata (Pokemon, Normal):
     def atacar (self, pokemon, tipo: str):
         if tipo.capitalize() == Normal.nombre:
             self.atk = Normal.get_ataque(self)
-            pokemon.recibir_danho(self.atk.atacar())
+            return pokemon.recibir_danho(self.atk.atacar())
         else:
             return ("Ataque no valido")
 
@@ -45,10 +45,10 @@ class Machop (Pokemon, Normal, Lucha):
     def atacar (self, pokemon, tipo: str):
         if tipo.capitalize() == Lucha.nombre:
             self.atk = Lucha.get_ataque(self)
-            pokemon.recibir_danho(self.atk.atacar())
+            return pokemon.recibir_danho(self.atk.atacar())
         elif tipo.capitalize() == Normal.nombre:
             self.atk = Normal.get_ataque(self)
-            pokemon.recibir_danho(self.atk.atacar())
+            return pokemon.recibir_danho(self.atk.atacar())
         else:
             return ("Ataque no valido")
         
@@ -61,9 +61,9 @@ class Venonat (Pokemon, Bicho, Veneno):
     def atacar (self, pokemon, tipo: str):
         if tipo.capitalize() == Bicho.nombre:
             atk = Bicho.get_ataque()
-            pokemon.recibir_danho(atk.atacar())
+            return pokemon.recibir_danho(atk.atacar())
         elif tipo.capitalize() == Veneno.nombre:
             atk = Veneno.get_ataque(self)
-            pokemon.recibir_danho(atk.atacar())
+            return pokemon.recibir_danho(atk.atacar())
         else:
             return("ataque no valido")
